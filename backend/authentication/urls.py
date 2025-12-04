b2f04cce8ad473d5948af3f55ebb202feb158e82
Ayananth <tsayananth93@gmail.com>
2025-12-04 22:27:08 +0530

    refresh token

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('sign_out', views.LogoutView.as_view(), name='sign_out'),
 
 
-    path('token/refresh', TokenRefreshView.as_view(), name='jwt_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
     path("verify-email/", views.VerifyEmailView.as_view(), name="verify_email"),
     path("resend-verification-email/", views.ResendVerificationEmailView.as_view(), name="resend_verification_email"),
     path("request-password-reset/", views.PasswordResetRequestView.as_view(), name="request-password-reset"),
