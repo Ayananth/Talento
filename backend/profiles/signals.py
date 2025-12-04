3a767e01b11ab68279d20fb457cb9aaa2da2f92e
Ayananth <tsayananth93@gmail.com>
2025-12-04 11:37:31 +0530

    created signals for profile creation

---
+++ backend/profiles/signals.py
@@ -0,0 +1,27 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import JobSeekerProfile, RecruiterProfile
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if not created:
+        return  # Only run on first creation
+
+    if instance.role == "jobseeker":
+        JobSeekerProfile.objects.create(
+            user=instance,
+            fullname=getattr(instance, "full_name", "") or getattr(instance, "username", "") or "",
+
+        )
+
+    # If recruiter
+    elif instance.role == "recruiter":
+        RecruiterProfile.objects.create(
+            user=instance,
+        )
