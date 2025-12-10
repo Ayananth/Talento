7a1376cafbf0c792be80749f15944ef22a769940
Ayananth <tsayananth93@gmail.com>
2025-12-10 09:48:51 +0530

    added routes for admin

--- frontend/src/app/routes/admin.routes.jsx
+++ frontend/src/app/routes/admin.routes.jsx
@@ -1,2 +1,32 @@
-const adminRoutes={}
-export default adminRoutes
+import Homepage from "../../features/jobseeker/home/Homepage";
+import RequireAuth from "../../features/routes/RequireAuth";
+import RoleRoute from "../../features/routes/RoleRoute";
+import RecruiterListPage from "../../features/admin/recruiters/RecruiterListPage";
+import AdminLayout from "../../features/admin/layout/AdminLayout";
+import { Children } from "react";
+import Dashboard from "../../features/admin/pages/Dashboard";
+
+
+const adminRoutes = [
+  {
+    path: "/admin",
+    element: (
+    //   <RequireAuth>
+        <AdminLayout />
+    //   </RequireAuth>
+    ),
+    children: [
+      {
+        element: <RoleRoute allowedRoles={["admin"]} />,
+        children: [
+          { path: "dashboard", element: <Dashboard /> },
+          { path: "recruiters", element: <RecruiterListPage /> }
+        ]
+      }
+    ]
+  }
+];
+
+
+
+export default adminRoutes;
