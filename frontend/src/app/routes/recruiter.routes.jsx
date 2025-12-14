35919d33b91d893b660bb2f71dbbb002c586299f
Ayananth <tsayananth93@gmail.com>
2025-12-14 13:45:53 +0530

    dummy pending page and route

--- frontend/src/app/routes/recruiter.routes.jsx
+++ frontend/src/app/routes/recruiter.routes.jsx
@@ -1,6 +1,8 @@
 import RecruiterOnboardingPage from "../../pages/recruiter/onboarding/RecruiterOnboardingPage"
+import VerificationPendingPage from "../../pages/recruiter/onboarding/VerificationPendingPage"
 
 const recruiterRoutes =[
-    {path:"recruiter/onboarding", element: <RecruiterOnboardingPage />}
+    {path:"recruiter/onboarding", element: <RecruiterOnboardingPage />},
+    {path:"recruiter/verification-pending", element: <VerificationPendingPage />}
 ]
 export default recruiterRoutes
---
+++ frontend/src/pages/recruiter/onboarding/VerificationPendingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VerificationPendingPage = () => {
+  return (
+    <div>
+      <h1>Verification pending</h1>
+    </div>
+  )
+}
+
+export default VerificationPendingPage
