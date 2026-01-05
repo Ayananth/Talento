6eee5d7cbd973700fa8f93e95667a211b29a60f1
Ayananth <tsayananth93@gmail.com>
2026-01-05 11:49:59 +0530

    backend modified to accept the job filter for recruiter in the applications list page

--- backend/applications/filters.py
+++ backend/applications/filters.py
@@ -3,7 +3,8 @@ from .models import JobApplication
 
 class JobApplicationFilter(django_filters.FilterSet):
     status = django_filters.CharFilter(field_name="status", lookup_expr="iexact")
+    job = django_filters.NumberFilter(field_name="job__id")
 
     class Meta:
         model = JobApplication
-        fields = ["status"]
+        fields = ["status", "job"]
