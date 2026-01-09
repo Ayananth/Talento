a6943e265cb24e8ba1f01f2300b7f7afd8b36698
Ayananth <tsayananth93@gmail.com>
2026-01-09 14:26:25 +0530

    web socket connected

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,40 +1,11 @@
-# chat/consumers.py
-import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import sync_to_async
-from .models import Message
-
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.conversation_id = self.scope["url_route"]["kwargs"]["conversation_id"]
-        self.room_group_name = f"chat_{self.conversation_id}"
-
-        await self.channel_layer.group_add(
-            self.room_group_name, self.channel_name
-        )
         await self.accept()
 
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-
-        message = await sync_to_async(Message.objects.create)(
-            conversation_id=self.conversation_id,
-            sender=self.scope["user"],
-            content=data["message"],
-        )
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "chat_message",
-                "message": data["message"],
-                "sender": self.scope["user"].id,
-            },
-        )
-
-    async def chat_message(self, event):
-        await self.send(text_data=json.dumps(event))
-
     async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
         pass
