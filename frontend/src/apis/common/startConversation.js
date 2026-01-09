00248ce1b944ab8c816d5b8ec21f715ae69140cc
Ayananth <tsayananth93@gmail.com>
2026-01-09 19:51:09 +0530

    start conversation js api

---
+++ frontend/src/apis/common/startConversation.js
@@ -0,0 +1,10 @@
+import api from "@/apis/api";
+export async function startConversation({ jobId, recipientId, content }) {
+  const res = await api.post("v1/chat/conversations/start/", {
+    job_id: jobId,
+    recipient_id: recipientId,
+    content,
+  });
+
+  return res.data;
+}
