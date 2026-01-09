9e98498b43fe68d3542c26150d9894883b31d7d3
Ayananth <tsayananth93@gmail.com>
2026-01-09 14:36:02 +0530

    web socket connection for each conversation

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -2,6 +2,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.conversation_id = self.scope["url_route"]["kwargs"]["conversation_id"]
         await self.accept()
 
     async def disconnect(self, close_code):
