fd279a119df87ed84d861d089a4099ca5c1d2398
Ayananth <tsayananth93@gmail.com>
2026-02-23 23:09:04 +0530

    api call url change in front end

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,5 +1,7 @@
 // src/constants.js
-export const API_BASE_URL ="http://127.0.0.1:8002";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const RAZORPAY_KEY_ID = import.meta.env.VITE_RAZORPAY_KEY_ID;
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 export const ACCESS_TOKEN = "access_token";
 export const REFRESH_TOKEN = "refresh_token";
 export const PAGE_SIZE = "12"
