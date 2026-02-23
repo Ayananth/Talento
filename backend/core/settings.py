4e3c7af340bd1ad0437fbd5e1b96eefede9fa3a3
Ayananth <tsayananth93@gmail.com>
2026-02-23 23:22:48 +0530

    CORS_ALLOWED_ORIGINS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -198,7 +198,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://ayananth.xyz",
-    "https://talento-dun.vercel.app/"    
+    "https://talento-dun.vercel.app"    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
