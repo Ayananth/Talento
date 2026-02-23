3b2f3e1bf222922b7c4a0bcd7380ff56eae09d3b
Ayananth <tsayananth93@gmail.com>
2026-02-23 23:08:32 +0530

    CORS_ALLOWED_ORIGINS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -197,6 +197,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://ayananth.xyz",
+    "https://talento-dun.vercel.app/"    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
