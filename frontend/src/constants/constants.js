9e3a9008b17981d3d20a68840a7a4f374e038a81
Ayananth <tsayananth93@gmail.com>
2026-02-24 08:31:43 +0530

    ws url from env

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -2,6 +2,7 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const RAZORPAY_KEY_ID = import.meta.env.VITE_RAZORPAY_KEY_ID;
 export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+export const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL;
 export const ACCESS_TOKEN = "access_token";
 export const REFRESH_TOKEN = "refresh_token";
 export const PAGE_SIZE = "12"
